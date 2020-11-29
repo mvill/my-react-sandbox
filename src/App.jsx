9ca822360cf5f3fda17175d56d3693e6768708a6
mvill <mathias.villeger@gmail.com>
2020-11-29 17:06:22 +0100

    mise en place de react-router suite

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Link } from 'react-router-dom';
 import routes from './RouteConfig';
 
 function App() {
   return (
     <Router>
       <div>
-        <div>header</div>
-        {routes.map(route => (
+        <div>
+          {
+            routes.map((route) => (
+              <Link
+                style={{ margin: '10px' }}
+                to={route.path}
+                key={`link-${route.path}`}
+              >
+                {route.path}
+              </Link>
+            ))
+          }
+        </div>
+        {routes.map((route) => (
           <Route
+            key={route.path}
             path={route.path}
             render={() => (<route.component />)}
           />
         ))}
+        <Redirect from="/" to="/home" />
         <div> coucou </div>
       </div>
     </Router>
